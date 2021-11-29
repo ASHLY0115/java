d38fc0887ef2619d62b5d98ea4e77e2101e8d0bf
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2021-11-29 15:52:22 +0530

    Add files via upload

---
+++ Ifcondition.java
@@ -0,0 +1,15 @@
+
+public class Ifcondition {
+
+	public static void main(String[] args) {
+		int a= 10,b=20;
+		//using else-if condition
+		if (a>b) {
+		
+		 System.out.println("largest="+a);
+		}
+		else {
+			System.out.println("largest="+b);
+     	} 
+   }
+}
