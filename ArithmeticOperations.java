d7d6d564a1e2a789c16f290d9ec5a10b190a2aa0
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2021-11-29 15:51:19 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,31 @@
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+		
+		int number1=10,number2=20;
+		int sum=0, difference=0,product=0,remainder=0;
+		float quotient;
+		quotient = (float)number1/number2;
+		
+		
+		
+		sum=number1+number2;
+		difference=number1-number2;
+		product=number1*number2;
+		
+		remainder=number1%number2;
+		
+		System.out.println("Sum ="+sum);
+		System.out.println("difference ="+difference);
+		System.out.println("quotient="+quotient);
+		System.out.println("product ="+product);
+		
+		System.out.println("remainder="+remainder);
+		
+		
+		
+
+	}
+
+}
