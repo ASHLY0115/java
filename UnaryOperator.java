ffc4cd16d842e2493cbf21388e0d013236c77335
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2021-11-29 15:52:35 +0530

    Add files via upload

---
+++ UnaryOperator.java
@@ -0,0 +1,12 @@
+
+public class UnaryOperator {
+
+	public static void main(String[] args) {
+    int counter = +1;
+    System.out.println(counter);
+    counter++;
+    System.out.println(counter);
+
+	}
+
+}
