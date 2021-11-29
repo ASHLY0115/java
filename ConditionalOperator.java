2a8bdabce4b42cbb45782c3e322ce85308226921
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2021-11-29 15:51:57 +0530

    Add files via upload

---
+++ ConditionalOperator.java
@@ -0,0 +1,12 @@
+
+public class ConditionalOperator {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		int max= a>b ? a:b;
+		
+		System.out.println("the maximum no is "+max);
+
+	}
+
+}
