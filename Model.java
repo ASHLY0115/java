232c4c9293ebc515a77f6c7aeb435fd07c31c050
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2022-02-08 14:58:48 +0530

    temporary 2

--- Model.java
+++ Model.java
@@ -1,16 +1,17 @@
+package test;
 
-
-import javax.swing.table.DefaultTableModel;
-
-/**
- * @author ashraf
- *
- */
-@SuppressWarnings("serial")
-public class Model extends DefaultTableModel {
-
-	public Model() {
-		super(Constants.DATA, Constants.TABLE_HEADER);
-	}
-
+public class Model {
+	private String firstname;
+	 private String lastname; public Model(String firstname, String lastname) {
+	  this.firstname = firstname;
+	  this.lastname = lastname;
+	 } public String getFirstname() {
+	  return firstname;
+	 } public void setFirstname(String firstname) {
+	  this.firstname = firstname;
+	 } public String getLastname() {
+	  return lastname;
+	 } public void setLastname(String lastname) {
+	  this.lastname = lastname;
+	 }
 }
--- View.java
+++ View.java
@@ -1,57 +1,88 @@
+package test;
 
-import java.awt.Dimension;
 
-import javax.swing.BorderFactory;
+import java.awt.BorderLayout;import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
-import javax.swing.JTable;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.border.TitledBorder;
-
-/**
- * @author ashraf
- *
- */
-public class View {
-
-	public View() {
-		// Create views swing UI components 
-		JTextField searchTermTextField = new JTextField(26);
-		JButton filterButton = new JButton("Filter");
-		JTable table = new JTable();
-
-		// Create table model
-		Model model = new Model();
-		table.setModel(model);
-
-		// Create controller
-		Controller controller = new Controller(searchTermTextField, model);
-		filterButton.addActionListener(controller);
-
-		// Set the view layout
-		JPanel ctrlPane = new JPanel();
-		ctrlPane.add(searchTermTextField);
-		ctrlPane.add(filterButton);
-
-		JScrollPane tableScrollPane = new JScrollPane(table);
-		tableScrollPane.setPreferredSize(new Dimension(700, 182));
-		tableScrollPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Market Movers",
-				TitledBorder.CENTER, TitledBorder.TOP));
-
-		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, ctrlPane, tableScrollPane);
-		splitPane.setDividerLocation(35);
-		splitPane.setEnabled(false);
-
-		// Display it all in a scrolling window and make the window appear
-		JFrame frame = new JFrame("Swing MVC Demo");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(splitPane);
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}
-
-}
+import javax.swing.SwingConstants;public class View { // View uses Swing framework to display UI to user
+ private JFrame frame;
+ private JLabel firstnameLabel;
+ private JLabel lastnameLabel;
+ private JTextField firstnameTextfield;
+ private JTextField lastnameTextfield;
+ private JButton firstnameSaveButton;
+ private JButton lastnameSaveButton;
+ private JButton hello;
+ private JButton bye; public View(String title) {
+  frame = new JFrame(title);
+  frame.getContentPane().setLayout(new BorderLayout());
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  frame.setSize(500, 120);
+  frame.setLocationRelativeTo(null);
+  frame.setVisible(true);  // Create UI elements
+  firstnameLabel = new JLabel("Firstname :");
+  lastnameLabel = new JLabel("Lastname :");
+  firstnameTextfield = new JTextField();
+  lastnameTextfield = new JTextField();
+  firstnameSaveButton = new JButton("Save firstname");
+  lastnameSaveButton = new JButton("Save lastname");
+  hello = new JButton("Hello!");
+  bye = new JButton("Bye!");  // Add UI element to frame
+  GroupLayout layout = new GroupLayout(frame.getContentPane());
+  layout.setAutoCreateGaps(true);
+  layout.setAutoCreateContainerGaps(true);
+  layout.setHorizontalGroup(layout.createSequentialGroup()
+    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(firstnameLabel)
+    .addComponent(lastnameLabel))
+    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(firstnameTextfield)
+    .addComponent(lastnameTextfield))
+    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(firstnameSaveButton)
+    .addComponent(lastnameSaveButton))
+    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(hello)
+    .addComponent(bye)));
+  layout.setVerticalGroup(layout.createSequentialGroup()
+    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(firstnameLabel)
+    .addComponent(firstnameTextfield).addComponent(firstnameSaveButton).addComponent(hello))
+    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(lastnameLabel)
+    .addComponent(lastnameTextfield).addComponent(lastnameSaveButton).addComponent(bye)));  layout.linkSize(SwingConstants.HORIZONTAL, firstnameSaveButton, lastnameSaveButton);
+  layout.linkSize(SwingConstants.HORIZONTAL, hello, bye);
+  frame.getContentPane().setLayout(layout);
+ } public JFrame getFrame() {
+  return frame;
+ } public void setFrame(JFrame frame) {
+  this.frame = frame;
+ } public JLabel getFirstnameLabel() {
+  return firstnameLabel;
+ } public void setFirstnameLabel(JLabel firstnameLabel) {
+  this.firstnameLabel = firstnameLabel;
+ } public JLabel getLastnameLabel() {
+  return lastnameLabel;
+ } public void setLastnameLabel(JLabel lastnameLabel) {
+  this.lastnameLabel = lastnameLabel;
+ } public JTextField getFirstnameTextfield() {
+  return firstnameTextfield;
+ } public void setFirstnameTextfield(JTextField firstnameTextfield) {
+  this.firstnameTextfield = firstnameTextfield;
+ } public JTextField getLastnameTextfield() {
+  return lastnameTextfield;
+ } public void setLastnameTextfield(JTextField lastnameTextfield) {
+  this.lastnameTextfield = lastnameTextfield;
+ } public JButton getFirstnameSaveButton() {
+  return firstnameSaveButton;
+ } public void setFirstnameSaveButton(JButton firstnameSaveButton) {
+  this.firstnameSaveButton = firstnameSaveButton;
+ } public JButton getLastnameSaveButton() {
+  return lastnameSaveButton;
+ } public void setLastnameSaveButton(JButton lastnameSaveButton) {
+  this.lastnameSaveButton = lastnameSaveButton;
+ } public JButton getHello() {
+  return hello;
+ } public void setHello(JButton hello) {
+  this.hello = hello;
+ } public JButton getBye() {
+  return bye;
+ } public void setBye(JButton bye) {
+  this.bye = bye;
+ }}
