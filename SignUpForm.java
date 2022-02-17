e6061cb5a3d9a71245db605ce782c13e484d0dd5
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2022-02-17 10:14:39 +0530

    temp bt crrt

--- SignUpForm.java
+++ SignUpForm.java
@@ -1,48 +1,63 @@
-import java.awt.*;
+package swing;
+
+import java.awt.Frame;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.FlowLayout;
 
-public class SignUpForm extends Frame implements ActionListener {
-	Button button;
-	TextField textField1 = new TextField(30);
-	TextField textField2 = new TextField(30);
-	TextField textField3 = new TextField(30);
-	TextField textField4 = new TextField(30);
-	
-	SignUpForm(){
-		
-		setTitle("Sign up page");
-		setSize(300,400);
-		setVisible(true);
-		setLayout(new FlowLayout());
-		button=new Button("SUBMIT");
-		add(textField1);
-		add(textField2);
-		add(textField3);
-		add(textField4);
-		
-		add(button);
-		textField1.setText("Name");
-		textField2.setText("email");
-		textField3.setText("Mob Number");
-		textField4.setText("place");
-		
-		button.addActionListener(this);
-		
-		
-	}
-
-public void actionPerformed(ActionEvent e) {
-		
-		System.out.println("Name :"+textField1.getText());
-		System.out.println(" email: "+textField2.getText());
-		System.out.println("Mob Number  :-"+textField3.getText());
-		System.out.println("place : "+textField4.getText());
-		
-		
-	}
-
-public static void main(String[] args) {
-	new LoginPageDemo();
-}
+public class SignUpForm extends Frame implements ActionListener{
+    TextField textField1 =new TextField(20);
+    TextField textField2 =new TextField(20);
+    TextField textField3 =new TextField(20);
+    TextField textField4 =new TextField(20);
+    
+    private Label name,userName,email,password;
+    
+    Button button;
+    SignUpForm(){
+        //Frame setting
+        setTitle("Sign Up Here");
+        setSize(380,250);
+        setVisible(true);
+        setLayout(new FlowLayout());
+        //name
+        name=new Label("Name        :");
+        name.setBounds(50,80,100,50);
+        add(name);
+        add(textField1);
+        //email
+        email=new Label("Email       :");
+        email.setBounds(50,100,100,50);
+        add(email);
+        add(textField2);
+        //user name
+        userName=new Label("User Name:");
+        userName.setBounds(50,120,100,50);
+        add(userName);
+        add(textField3);
+        //password
+        password=new Label("Password :");
+        password.setBounds(50,140,100,50);
+        add(password);
+        add(textField4);
+        //button
+        button=new Button("Sign Up");
+        button.setBounds(150,160,60,30);
+        add(button);    
+        button.addActionListener(this);
+  
+    }
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Name       :"+textField1.getText());
+		System.out.println("Email ID   :"+textField2.getText());
+		System.out.println("User Name  :"+textField3.getText());
+		System.out.println("Password   : "+textField4.getText());
+        
+    }
+    public static void main(String[] args){
+        SignUpForm form =new SignUpForm();
+    }
 }
