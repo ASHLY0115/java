47fde753bb03d8cad3e0111fe1b7edaa9095174c
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2022-02-18 14:31:37 +0530

    updated

--- stringtokenizer.java
+++ stringtokenizer.java
@@ -1,3 +1,10 @@
+/****************************************
+ * file    : string tokenizer
+ * authors : AshNav
+ ******************************************/
+
+
+
 package test2;
 
 import java.util.StringTokenizer;  
@@ -6,12 +13,18 @@ import java.util.Scanner;
 public class stringtokenizer{  
 	
  public static void main(String args[]){  
+	 int count=0;
 	 Scanner sc = new Scanner (System.in);
 	 System.out.println("Enter the string");
 	 String string = sc.nextLine();
-     StringTokenizer st = new StringTokenizer(string," ");  
+	 
+     StringTokenizer st = new StringTokenizer(string," "); 
+     
      while (st.hasMoreTokens()) {  
          System.out.println(st.nextToken());  
-     }  
+         count = count+1;
+         
+     }     
+     System.out.println("no of counts"+ count);
    }  
 }  
